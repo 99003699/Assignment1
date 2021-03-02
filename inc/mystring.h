3a8f21d1d19757f59e77647b8f14a15b77aa2914
99003699 <78849857+99003699@users.noreply.github.com>
2021-03-02 13:54:45 +0530

    Create mystring.h

---
+++ inc/mystring.h
@@ -0,0 +1,12 @@
+#ifndef MYSTRING_DOT_H
+#define MYSTRINGDOT_H
+#include<string.h>
+
+int factorial(int num);
+int isPalindrome(int palind);
+int isPrime(int number);
+int vsum(int arrnum[]);
+
+
+
+#endif
