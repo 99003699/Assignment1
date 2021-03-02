9058559e0529b70bc0b0fe0515cb91d2d7e407ab
99003699 <78849857+99003699@users.noreply.github.com>
2021-03-02 14:02:16 +0530

    Create test.c

---
+++ test.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <myutils.h>
+#include <mystring.h>
+
+
+int main(){
+    int first =  factorial(5);
+    printf("%d", first);
+    printf("%d", isPrime(4));
+    printf("%d", isPalindrome(1001));
+    printf("%d", vsum(arraynum ={1, 2, 3, 4, 5, 6}));
+    printf("%d", mystrlen("Hello"));
+    printf("%s", mystrcpy("Hello"));
+    printf("%d", mystrcmp("Hello", "world"));
+    printf("%s", mystrcat("Hello", "world"));
+
+
+
+
+    /*****************************************************************************/
+
+    printf("%d", mystrlen("Hello"));
+    printf("%s", mystrcpy("Hello"));
+    printf("%d", mystrcmp("Hello", "world"));
+    printf("%s", mystrcat("Hello", "world"));
+
+    /***********************************************************************************
